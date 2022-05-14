2220ba3949c04104875e47c0352bde860d5ac1b2
viwnu <xenmysafitiplace@gmail.com>
2022-05-14 19:05:55 +0300

    done preparing sample files

--- mainPageSlider.js
+++ mainPageSlider.js
@@ -1,20 +1,13 @@
-let roomsSliderController = {
-  sliderItem: document.querySelectorAll(".rooms-list__item"),
-  sliderContainer: document.querySelector(".rooms-list"),
-};
-
-let fidbackSliderController =  {
-  sliderItem: document.querySelectorAll(".fidback__text"),
-  sliderContainer: document.querySelector(".fidback__slider"),
-  arrows: document.querySelectorAll('.fidback__controller'),
+let DOMReference = {
+  sliderItem: document.querySelectorAll(".list__item"),
+  sliderContainer: document.querySelector(".list"),
+  arrows: document.querySelectorAll('.arrows'),
   buttonNames: {
-    next: document.querySelector('.fidback__controller > button:nth-child(1)').name,
-    prev: document.querySelector('.fidback__controller > button:nth-child(2)').name
+    next: document.querySelector('.arrows > button:nth-child(1)').name,
+    prev: document.querySelector('.arrows > button:nth-child(2)').name
   }
 };
 
-let roomsSlider = slider(roomsSliderController);
-let fidbackSlider = slider(fidbackSliderController);
+let OnPageSlider = slider(DOMReference);
 
-roomsSlider();
-fidbackSlider();
+OnPageSlider();
